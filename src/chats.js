b238b07ca50577179ed496475eb28e90255e9ec7
Olga <89293323+ovdemina@users.noreply.github.com>
2022-05-14 23:24:09 +0300

    dz3
    автофокус, массив чатов, Material UI

---
+++ src/chats.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+export function InsetList() {
+  return (
+    <List
+      sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+      aria-label="contacts"
+    >
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemText inset primary="Ivan Ivanov" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemText inset primary="Alexey Demin" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemText inset primary="Anna Kivina" />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
+}
