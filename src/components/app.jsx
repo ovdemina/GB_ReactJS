9c95819d8b8f76764b7aee855a5b1c11821bbcfa
Olga <89293323+ovdemina@users.noreply.github.com>
2022-05-26 10:43:27 +0300

    счетчик count Redux

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,9 +5,22 @@ import { Chats } from "../pages/chats";
 import { NotFound } from "../pages/notFound";
 import { Profile } from "../pages/profile";
 import { Home } from "./home";
+import { useDispatch, useSelector } from "react-redux";
+
+
 
 export function App() {
+  const count = useSelector(state => state.count)
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch ({type: "increase"})
+  }
+  const decrease = () => {
+    dispatch ({type: "decrease"})
+  }
   return (
+    <>
         <Routes>
             <Route path={"/"} element={<Home />}>
                 <Route index element={<Profile />} />
@@ -16,5 +29,15 @@ export function App() {
                 <Route path="/*" element={<NotFound />} />
             </Route>
         </Routes>
+        <div className="count">
+          <div>
+            <button className="countButton" onClick={decrease}>-</button>
+          </div>
+          {count}
+          <div>
+            <button className="countButton" onClick={increase}>+</button>
+          </div>
+        </div>
+        </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/app";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const initialState = {
+  count: 0,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increase":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "decrease":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
